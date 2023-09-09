41913b4f91b30221bc35be251d1ad41b89f8283c
Parle648 <sashahoumi@gmail.com>
2023-09-09 16:30:21 +0300

    add scroll header

--- client/src/widgets/Header/HeroSection.tsx
+++ client/src/widgets/Header/HeroSection.tsx
@@ -6,20 +6,27 @@ import burgerTrigger from '../../img/burger.svg';
 import background from '../../img/hero-section.png';
 import cross from '../../img/cross.svg';
 import backgroundMobile from '../../img/heromobile.png';
+import text from '../../img/header-text.png';
 import styles from './styles/hero.module.scss';
 import '../../Scss/styles.scss';
 
 const HeroSection = () => {
     const [disabled, setDisabled] = React.useState(false);
+    const [scroll, setScroll] = React.useState(false);
 
     const toggleMenu = () => {
         setDisabled(!disabled)
     }
 
+    document.addEventListener('scroll', () => {
+        window.scrollY <=500 ? setScroll(false) : setScroll(true);
+    })
+    
+
     return (
         <div className={styles.heroSection}>
             <img className={styles.background} src={document.body.clientWidth > 800 ? background : backgroundMobile} alt="background" />
-            <header className={`${styles.header} flex justify-between items-start`}>
+            <header className={`${styles.header} flex justify-between items-start ${scroll && styles.scrollHeader}`}>
                 {document.body.clientWidth > 800 &&
                     <Link to='/'><Logo /></Link>
                 }
@@ -55,6 +62,7 @@ const HeroSection = () => {
                     </div>
                 </div>
 
+                <img className={`${styles.headerText} ${scroll && styles.scrolledText}`} src={text} alt="text" />
 
                 <nav className={`${styles.nav}`}>
                     <ul className={`${styles.navigationList} flex justify-between`}>
@@ -66,7 +74,10 @@ const HeroSection = () => {
                         <li className={`${styles.link} text-sm Oswald400 gray uppercase cursor-pointer`}><Link to=''></Link> Каталоssг</li>
                     </ul>
                 </nav>
-                <div className={styles.contactBlock}>
+
+                <a className={`${styles.number} Oswald400 green ${!scroll && styles.scrolledNumber}`} href="#!">+375 (29) 113-69-69</a>
+
+                <div className={`${styles.contactBlock} ${scroll && styles.scrolledContactBlock}`}>
                     <a className='uppercase Oswald400 green' href="#!">zakaz@loverflower.by</a>
                     <h2 className={`${styles.delivery} Oswald300 gray`}>Доставка 24/7 по договоренности с оператором</h2>
                     <h2 className={`${styles.street} Oswald400 green`}>ул. Тимирязева 67</h2>
