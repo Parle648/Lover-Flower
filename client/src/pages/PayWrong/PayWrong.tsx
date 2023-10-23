5d07b2ccbcc6da58161477f577f17fb9f9c80238
Parle648 <sashahoumi@gmail.com>
2023-10-23 10:43:37 +0300

    create deliveryPage

--- client/src/pages/PayWrong/PayWrong.tsx
+++ client/src/pages/PayWrong/PayWrong.tsx
@@ -11,23 +11,23 @@ import backgroundMobile from '../../img/pay-wrong-mobile.png';
 const PayWrong = () => {
     return (
         <div className={styles.block}>
-        <TopHeader/>
-        <img className={styles.background} src={document.body.clientWidth > 800 ? background : backgroundMobile} alt="" />
-        <div className={`${styles.wrapper} wrapper`}>
-            <h2 className={styles.ttl}>Ошибка</h2>
-            <h2 className={styles.secondTtl}>платежа</h2>
-            <p className={styles.paragraph}>
-                Произошла ошибка....
-            </p>
-            <p className={styles.paragraph}>
-                Попробуйте оплатить еще раз!
-            </p>
+            <TopHeader/>
+            <img className={styles.background} src={document.body.clientWidth > 800 ? background : backgroundMobile} alt="" />
+            <div className={`${styles.wrapper} wrapper`}>
+                <h2 className={styles.ttl}>Ошибка</h2>
+                <h2 className={styles.secondTtl}>платежа</h2>
+                <p className={styles.paragraph}>
+                    Произошла ошибка....
+                </p>
+                <p className={styles.paragraph}>
+                    Попробуйте оплатить еще раз!
+                </p>
 
-            <button className='greenBtn'>Оплатить еще раз</button>
-            <a className={styles.toMain} href="/">Вернуться на главную</a>
+                <button className='greenBtn'>Оплатить еще раз</button>
+                <a className={styles.toMain} href="/">Вернуться на главную</a>
+            </div>
+            {document.body.clientWidth > 800 && <Footer />}
         </div>
-        {document.body.clientWidth > 800 && <Footer />}
-    </div>
     );
 };
 
