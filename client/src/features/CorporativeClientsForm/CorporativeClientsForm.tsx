b71d9e2eba6fc907883859a34e74408af4466b54
Parle648 <sashahoumi@gmail.com>
2024-01-02 18:04:30 +0200

    fix validation UI in QuestionForm and CorporativeClietnsForm

--- client/src/features/CorporativeClientsForm/CorporativeClientsForm.tsx
+++ client/src/features/CorporativeClientsForm/CorporativeClientsForm.tsx
@@ -35,16 +35,20 @@ const CorporativeClientsForm = () => {
         <form className={styles.form} onSubmit={handleSubmit(sendOrder)}>
             <div>
                 <h2 className={styles.inputTtl}>Наименование организации</h2>
-                <input className={`input ${errors?.organisationName && 'wrongValue'}`} placeholder='Введите наименование вашей организации' {...register("organisationName", {
-                    required: true,
-                    pattern: {
-                        value: /^[А-Яа-яЁё\s]+$/,
-                        message: 'Введите правильно название фирмы'
-                    }
-                })}/>
-                {errors?.organisationName && <h2 className='errorMessage'>Введите правильно название фирмы</h2>}
+                <label className='label'>
+                    <input className={`input ${errors?.organisationName && 'wrongValue'}`} placeholder='Введите наименование вашей организации' {...register("organisationName", {
+                        required: true,
+                        pattern: {
+                            value: /^[А-Яа-яЁё\s]+$/,
+                            message: 'Введите правильно название фирмы'
+                        }
+                    })}/>
+                    {errors?.organisationName && <h2 className='errorMessage'>Введите правильно название фирмы</h2>}
+                    {errors?.organisationName && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Почтовый адрес</h2>
+                <label className='label'>
                 <input className={`input ${errors?.mailIndex && 'wrongValue'}`} placeholder='Введите почтовый адрес' {...register("mailIndex", {
                     required: true,
                     pattern: {
@@ -53,8 +57,10 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.mailIndex && <h2 className='errorMessage'>Введите правильно почт индекс</h2>}
-                
+                {errors?.mailIndex && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>             
                 <h2 className={styles.inputTtl}>Контактное лицо</h2>
+                <label className='label'>
                 <input className={`input ${errors?.contactPerson && 'wrongValue'}`} placeholder='Введите имя контактного лица' {...register("contactPerson", {
                     required: true,
                     pattern: {
@@ -63,8 +69,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.contactPerson && <h2 className='errorMessage'>Введите правильно контактное лицо</h2>}
+                {errors?.contactPerson && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Контактный номер телефона</h2>
+                <label className='label'>
                 <input className={`input ${errors?.contactNumber && 'wrongValue'}`} placeholder='+7 (977) 777-77-77' {...register("contactNumber", {
                     required: 'Поле имя є обов\'язковим',
                     pattern: {
@@ -73,14 +82,20 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.contactNumber && <h2 className='errorMessage'>поле имя необходимо заполнить видом +7990000000</h2>}
+                {errors?.contactNumber && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Стоимость букета сотруднику (если разная – указать)</h2>
+                <label className='label'>
                 <input className={`input ${errors?.busketAmount && 'wrongValue'}`} placeholder='Укажите стоимость букета сотруднику' {...register("busketAmount", {
                     required: true,
                 })}/>
                 {errors?.busketAmount && <h2 className='errorMessage'>Введите стоимость</h2>}
+                {errors?.busketAmount && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Адрес электронной почты</h2>
+                <label className='label'>
                 <input className={`input ${errors?.emailAdress && 'wrongValue'}`} placeholder='Укажите ваш адрес электронной почты' {...register("emailAdress", {
                     required: 'Поле имя є обов\'язковим',
                     pattern: {
@@ -89,9 +104,12 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.emailAdress && <h2 className='errorMessage'>Будь ласка, введіть правильний адресу електронної пошти</h2>}
+                {errors?.emailAdress && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 {document.body.clientWidth < 800 && <div>
                 <h2 className={styles.inputTtl}>Введите УНП</h2>
+                <label className='label'>
                 <input className={`input ${errors?.YNP && 'wrongValue'}`} placeholder='УНП' {...register("YNP", {
                     required: true,
                     pattern: {
@@ -100,8 +118,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.YNP && <h2 className='errorMessage'>Введите УНП</h2>}
+                {errors?.YNP && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Расчетный счет</h2>
+                <label className='label'>
                 <input className={`input ${errors?.currentAccount && 'wrongValue'}`} placeholder='Введите номер расчетного счета' {...register("currentAccount", {
                     required: true,
                     pattern: {
@@ -110,8 +131,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.currentAccount && <h2 className='errorMessage'>Введите pасчетный счет</h2>}
+                {errors?.currentAccount && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Код банка</h2>
+                <label className='label'>
                 <input className={`input ${errors?.bankCode && 'wrongValue'}`} placeholder='Код банка' {...register("bankCode", {
                     required: true,
                     pattern: {
@@ -120,8 +144,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.bankCode && <h2 className='errorMessage'>Введите код банка</h2>}
+                {errors?.bankCode && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Предполагаемое количество заявок в месяц</h2>
+                <label className='label'>
                 <input className={`input ${errors?.countOfOrders && 'wrongValue'}`} placeholder='Введите предполагаемое количество заявок в месяц' {...register("countOfOrders", {
                     required: true,
                     pattern: {
@@ -130,6 +157,8 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.countOfOrders && <h2 className='errorMessage'>Введите предполагаемое количество заявок в месяц</h2>}
+                {errors?.countOfOrders && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
             </div>}
 
@@ -139,6 +168,7 @@ const CorporativeClientsForm = () => {
 
                 {document.body.clientWidth > 800 && <div>
                 <h2 className={styles.inputTtl}>Введите УНП</h2>
+                <label className='label'>
                 <input className={`input ${errors?.YNP && 'wrongValue'}`} placeholder='УНП' {...register("YNP", {
                     required: true,
                     pattern: {
@@ -147,8 +177,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.YNP && <h2 className='errorMessage'>Введите УНП</h2>}
+                {errors?.YNP && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Расчетный счет</h2>
+                <label className='label'>
                 <input className={`input ${errors?.currentAccount && 'wrongValue'}`} placeholder='Введите номер расчетного счета' {...register("currentAccount", {
                     required: true,
                     pattern: {
@@ -157,8 +190,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.currentAccount && <h2 className='errorMessage'>Введите pасчетный счет</h2>}
+                {errors?.currentAccount && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Код банка</h2>
+                <label className='label'>
                 <input className={`input ${errors?.bankCode && 'wrongValue'}`} placeholder='Код банка' {...register("bankCode", {
                     required: true,
                     pattern: {
@@ -167,8 +203,11 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.bankCode && <h2 className='errorMessage'>Введите код банка</h2>}
+                {errors?.bankCode && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
                 <h2 className={styles.inputTtl}>Предполагаемое количество заявок в месяц</h2>
+                <label className='label'>
                 <input className={`input ${errors?.countOfOrders && 'wrongValue'}`} placeholder='Введите предполагаемое количество заявок в месяц' {...register("countOfOrders", {
                     required: true,
                     pattern: {
@@ -177,6 +216,8 @@ const CorporativeClientsForm = () => {
                     }
                 })}/>
                 {errors?.countOfOrders && <h2 className='errorMessage'>Введите предполагаемое количество заявок в месяц</h2>}
+                {errors?.countOfOrders && <h2 className='errorMessage exlamation'>!</h2>}
+                </label>
 
             </div>}
         </form>
--- client/src/features/HaveQuestionForm/HaveQuestionForm.tsx
+++ client/src/features/HaveQuestionForm/HaveQuestionForm.tsx
@@ -33,30 +33,39 @@ const HaveQuestionForm = () => {
     
     return (
         <form className={styles.form} onSubmit={handleSubmit(orderCall)}>
-            <input  className={`input ${errors?.name && 'wrongValue'}`} type="text" placeholder='Ваше имя' id='' {...register('name', {
-                required: true,
-                pattern: {
-                    value: /[^\w\d\s]/u,
-                    message: 'Введите ваше имя',
-                }
-            })}/>
-            {errors?.name && <h2 className='errorMessage'>Введите ваше имя</h2>}
-            <input className={`input ${errors?.number && 'wrongValue'}`} type="number" placeholder='+7 (977) 777-77-77' id='' {...register('number', {
-                required: true,
-                pattern: {
-                    value: /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
-                    message: 'Введите ваше имя',
-                }
-            })}/>
-            {errors?.number && <h2 className='errorMessage'>Введите ваш номер телефона</h2>}
-            <input className={`input ${errors?.message && 'wrongValue'}`} placeholder='Ваш комментарий' id='' {...register('message', {
-                required: true,
-                pattern: {
-                    value: /[^\w\d\s]/u,
-                    message: 'Введите ваше имя',
-                }
-            })}/>
-            {errors?.message && <h2 className='errorMessage'>Введите ваш Комментарий</h2>}
+            <label className='label'>
+                <input  className={`input ${errors?.name && 'wrongValue'}`} type="text" placeholder='Ваше имя' id='' {...register('name', {
+                    required: true,
+                    pattern: {
+                        value: /[^\w\d\s]/u,
+                        message: 'Введите ваше имя',
+                    }
+                })}/>
+                {errors?.name && <h2 className='errorMessage'>Введите ваше имя</h2>}
+                {errors?.name && <h2 className='errorMessage exlamation'>!</h2>}
+            </label>
+            <label className='label'>
+                <input className={`input ${errors?.number && 'wrongValue'}`} type="number" placeholder='+7 (977) 777-77-77' id='' {...register('number', {
+                    required: true,
+                    pattern: {
+                        value: /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
+                        message: 'Введите ваше имя',
+                    }
+                })}/>
+                {errors?.number && <h2 className='errorMessage'>Введите ваш номер телефона</h2>}
+                {errors?.number && <h2 className='errorMessage exlamation'>!</h2>}
+            </label>
+            <label className='label'>
+                <input className={`input ${errors?.message && 'wrongValue'}`} placeholder='Ваш комментарий' id='' {...register('message', {
+                    required: true,
+                    pattern: {
+                        value: /[^\w\d\s]/u,
+                        message: 'Введите ваше имя',
+                    }
+                })}/>
+                {errors?.message && <h2 className='errorMessage'>Введите ваш Комментарий</h2>}
+                {errors?.message && <h2 className='errorMessage exlamation'>!</h2>}
+            </label>
             <button className='greenBtn'>отправить</button>
             <p className={styles.policy}>
                 Нажимая  на кнопку «Отправить», я даю свое согласие на обработку персональных данных, в соответствии c <Link className={styles.link} to='/'>Политикой конфиденциальности</Link>
